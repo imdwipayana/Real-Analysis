27f2b9733be26824b4a65266d095e1fc444cd9e8
Eka Dwipayana <86109298+imdwipayana@users.noreply.github.com>
2022-09-22 21:15:41 +0800

    Add files via upload

---
+++ nxnkonvergen0.m
@@ -0,0 +1,21 @@
+clear all, close all, clc
+
+x=linspace(0,1,300);
+mu=0.99;
+
+z(1)=0.59;
+n=1:30;
+
+
+
+for i = 1:n(end)-1
+  
+    z(i+1)=mu*z(i)*(1-z(i))
+end
+
+  plot(n,z,'r--o',LineWidth=1,MarkerSize=4)
+  axis([0 n(end) 0 1])
+
+  xlabel('n')
+ylabel('X_n')
+  
